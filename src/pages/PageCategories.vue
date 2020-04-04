510fa30ffcf061914f956c1125aeaed96c0201c4
Romain BRETECHER <romain.bretecher@gmail.com>
2020-04-04 17:19:15 +0200

    feat: add category read only page

--- src/pages/PageCategories.vue
+++ src/pages/PageCategories.vue
@@ -1,10 +1,33 @@
 <template>
   <scroll-page>
     <big-title>Categories</big-title>
+
+    <q-list
+      bordered
+      separator
+    >
+      <q-item
+        clickable
+        v-ripple
+        v-for="(category, key) in categories"
+        :key="key"
+      >
+        <q-item-section avatar>
+          <q-icon
+            :name="category.icon"
+            color="green"
+          />
+
+        </q-item-section>
+        <q-item-section>{{ category.name }}</q-item-section>
+      </q-item>
+    </q-list>
+
   </scroll-page>
 </template>
 
 <script>
+import { mapGetters } from 'vuex';
 import ScrollPage from 'src/components/Shared/ScrollPage';
 import BigTitle from 'src/components/Shared/BigTitle';
 
@@ -12,6 +35,9 @@ export default {
   components: {
     ScrollPage,
     BigTitle,
+  },
+  computed: {
+    ...mapGetters('categories', ['categories']),
   },
 };
 </script>
--- src/store/categories/getters.js
+++ src/store/categories/getters.js
@@ -1,4 +1,26 @@
-/*
-export function someGetter (state) {
+export function categories(state) {
+  const categoriesOrdered = {};
+
+  const keys = Object.keys(state.categories);
+
+  keys.sort((a, b) => {
+    const categoryAProp = state.categories[a].name;
+    const categoryBProp = state.categories[b].name;
+
+    let result = 0;
+
+    if (categoryAProp > categoryBProp) {
+      result = 1;
+    } else if (categoryAProp < categoryBProp) {
+      result = -1;
+    }
+
+    return result;
+  });
+
+  keys.forEach((key) => {
+    categoriesOrdered[key] = state.categories[key];
+  });
+
+  return categoriesOrdered;
 }
-*/
