c67c543f46d7579ed2b40e8fc2d1300b1779ab1c
Romain BRETECHER <romain.bretecher@gmail.com>
2020-04-08 11:28:34 +0200

    doc: add some comments

--- functions/index.js
+++ functions/index.js
@@ -1,13 +1,9 @@
-// The Cloud Functions for Firebase SDK to create Cloud Functions and setup triggers.
 const functions = require('firebase-functions');
-
-// The Firebase Admin SDK to access the Firebase Realtime Database.
 const admin = require('firebase-admin');
 
 admin.initializeApp();
 
-// https://github.com/firebase/functions-samples/blob/master/child-count/functions/index.js
-
+// Update the collection count property when an expense is added/removed from a collection.
 exports.updateCount = functions.database.ref('/expenses/{collectionId}/{id}')
     .onWrite((change) => {
         const expensesRef = change.after.ref.parent;
@@ -19,29 +15,7 @@ exports.updateCount = functions.database.ref('/expenses/{collectionId}/{id}')
         });
     });
 
-/*
-exports.updateCountRecursive = functions.database.ref('/collections/{collectionId}/count')
-    .onUpdate(async (change) => {
-        const collectionRef = change.after.ref.parent;
-
-        const collectionSnapshot = await collectionRef.once('value');
-
-        // If the collection has a parent, it also needs to be updated.
-        if (collectionSnapshot.val().collection) {
-
-            const diff = change.after.val() - change.before.val();
-            const parentCollectionRef = admin.database().ref('/collections/' + collectionSnapshot.val().collection);
-
-            const parentCollectionSnapshot = await parentCollectionRef.once('value');
-            const newCount = (parentCollectionSnapshot.val().count || 0) + diff;
-            return parentCollectionRef.update({ count: newCount });
-        }
-
-        return null;
-    });
-    */
-
-
+// Update parent collection when a collection count is updated.
 exports.updateCountRecursive = functions.database.ref('/collections/{collectionId}/count')
     .onUpdate(async (change) => {
         const collectionRef = change.after.ref.parent;
