c1fb398d88bd37a39d9cc0e984081fac660d1704
Romain BRETECHER <romain.bretecher@gmail.com>
2020-04-08 18:33:17 +0200

    feat: add some content in the dashboard

---
+++ src/components/Dashboard/DashboardItem.vue
@@ -0,0 +1,21 @@
+<template>
+  <q-banner
+    inline-actions
+    rounded
+    class="bg-grey-3"
+  >
+    <template v-slot:avatar>
+      <q-icon
+        :name="icon"
+        color="primary"
+      />
+    </template>
+    <slot></slot>
+  </q-banner>
+</template>
+
+<script>
+export default {
+  props: ['icon'],
+};
+</script>
--- src/pages/PageDashboard.vue
+++ src/pages/PageDashboard.vue
@@ -1,17 +1,56 @@
 <template>
   <scroll-page>
     <big-title>Dashboard</big-title>
+
+    <div class="dashboard-item-list">
+
+      <dashboard-item icon="euro_symbol">
+        {{ expenseCount }} expenses
+      </dashboard-item>
+
+      <dashboard-item icon="account_tree">
+        {{ Object.keys(collections).length }} collection
+      </dashboard-item>
+
+      <dashboard-item icon="category">
+        {{ Object.keys(categories).length }} categories
+      </dashboard-item>
+
+      <dashboard-item icon="account_circle">
+        {{ Object.keys(users).length }} users
+      </dashboard-item>
+    </div>
   </scroll-page>
 </template>
 
 <script>
+import { mapState } from 'vuex';
 import ScrollPage from 'src/components/Shared/ScrollPage';
 import BigTitle from 'src/components/Shared/BigTitle';
+import DashboardItem from 'src/components/Dashboard/DashboardItem';
 
 export default {
   components: {
     ScrollPage,
     BigTitle,
+    DashboardItem,
+  },
+  computed: {
+    ...mapState('users', ['users']),
+    ...mapState('collections', ['collections']),
+    ...mapState('categories', ['categories']),
+    expenseCount() {
+      return Object
+        .keys(this.collections)
+        .filter((key) => !this.collections[key].collection)
+        .reduce((acc, currentKey) => acc + this.collections[currentKey].count, 0);
+    },
   },
 };
 </script>
+
+<style>
+.dashboard-item-list > * {
+  margin: 10px 0;
+}
+</style>
