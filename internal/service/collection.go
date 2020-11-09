8a83cafa5b0ac977dfa087c6cd12609e0829ba34
Romain Bretécher <romain.bretecher@gmail.com>
2020-11-09 22:51:33 +0100

    refactor: setup RPC API

--- internal/service/collection.go
+++ internal/service/collection.go
@@ -1,21 +1,44 @@
 package service
 
 import (
+	"net/http"
+
 	"github.com/jmoiron/sqlx"
 	"github.com/rbretecher/expense-tracker-back/internal/domain"
 )
 
-func GetCollections(db *sqlx.DB) ([]domain.Collection, error) {
-	collections := []domain.Collection{}
-	err := db.Select(&collections, "SELECT * FROM collections;")
-	return collections, err
+type CollectionService struct {
+	db *sqlx.DB
 }
 
-func CreateCollection(db *sqlx.DB, c domain.Collection) (domain.Collection, error) {
-	err := db.QueryRow(`
-		INSERT INTO collections (name, icon_name, icon_color)
-		VALUES ($1, $2, $3)
-		RETURNING collection_id
-	`, c.Name, c.IconName, c.IconColor).Scan(&c.ID)
-	return c, err
+func NewCollectionService(db *sqlx.DB) *CollectionService {
+	return &CollectionService{
+		db: db,
+	}
+}
+
+func (s *CollectionService) All(r *http.Request, args *NoArgs, reply *[]*domain.Collection) error {
+	return s.db.Select(reply, "SELECT * FROM collections")
+}
+
+type CreateArgs struct {
+	Name      string `json:"name"`
+	IconName  string `json:"iconName"`
+	IconColor string `json:"iconColor"`
+}
+
+func (s *CollectionService) Create(r *http.Request, args *CreateArgs, reply *domain.Collection) error {
+	return s.db.QueryRow(`
+			INSERT INTO collections (name, icon_name, icon_color)
+			VALUES ($1, $2, $3)
+			RETURNING collection_id, name, icon_name, icon_color
+		`, args.Name, args.IconName, args.IconColor).Scan(&reply.ID, &reply.Name, &reply.IconName, &reply.IconColor)
+}
+
+type GetArgs struct {
+	CollectionID int `json:"collection_id"`
+}
+
+func (s *CollectionService) Get(r *http.Request, args *GetArgs, reply *domain.Collection) error {
+	return s.db.Get(reply, "SELECT * FROM collections WHERE collection_id = $1", args.CollectionID)
 }
