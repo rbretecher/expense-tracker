b926553da6d2b18fafc3a6a727accae3c1828d9e
Romain Bretécher <romain.bretecher@gmail.com>
2020-11-11 00:30:30 +0100

    refactor: move app into dedicated package

---
+++ internal/app/app.go
@@ -0,0 +1,40 @@
+package app
+
+import (
+	"net/http"
+
+	"github.com/gorilla/rpc/v2"
+	"github.com/gorilla/rpc/v2/json2"
+	"github.com/jmoiron/sqlx"
+	"github.com/rbretecher/expense-tracker-back/internal/infrastructure/database"
+	"github.com/rbretecher/expense-tracker-back/internal/service"
+	"github.com/rbretecher/expense-tracker-back/pkg/validator"
+)
+
+type app struct {
+	server *rpc.Server
+	db     *sqlx.DB
+}
+
+func New() app {
+	db := database.Connect()
+
+	s := rpc.NewServer()
+
+	s.RegisterCodec(json2.NewCodec(), "application/json")
+	s.RegisterCodec(json2.NewCodec(), "application/json;charset=UTF-8")
+	s.RegisterValidateRequestFunc(func(r *rpc.RequestInfo, i interface{}) error {
+		return validator.Validate.Struct(i)
+	})
+	s.RegisterService(service.NewCollectionService(db), "Collection")
+
+	return app{
+		server: s,
+		db:     db,
+	}
+}
+
+func (a app) Start() {
+	http.Handle("/rpc", a.server)
+	http.ListenAndServe(":8080", nil)
+}
