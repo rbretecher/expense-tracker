9626b8594a057effa558cdb8a20ddb1b884e9482
Romain Bretécher <romain.bretecher@gmail.com>
2020-11-10 23:27:54 +0100

    refactor(db): use id for primary keys

--- docker-entrypoint-initdb.d/init.sql
+++ docker-entrypoint-initdb.d/init.sql
@@ -1,6 +1,6 @@
 CREATE TABLE users
 (
-    user_id INT GENERATED ALWAYS AS IDENTITY,
+    id INT GENERATED ALWAYS AS IDENTITY,
     name VARCHAR (255) NOT NULL,
     email VARCHAR (255) NOT NULL,
     password VARCHAR (255) NOT NULL,
@@ -8,38 +8,38 @@ CREATE TABLE users
     icon_name VARCHAR (255) NOT NULL,
     icon_color VARCHAR (255) NOT NULL,
 
-    PRIMARY KEY(user_id)
+    PRIMARY KEY(id)
 );
 
 CREATE TABLE collections
 (
-    collection_id INT GENERATED ALWAYS AS IDENTITY,
+    id INT GENERATED ALWAYS AS IDENTITY,
     name VARCHAR (255) NOT NULL,
     icon_name VARCHAR (255) NOT NULL,
     icon_color VARCHAR (255) NOT NULL,
 
-    PRIMARY KEY(collection_id)
+    PRIMARY KEY(id)
 );
 
 CREATE TABLE categories
 (
-    category_id INT GENERATED ALWAYS AS IDENTITY,
+    id INT GENERATED ALWAYS AS IDENTITY,
     name VARCHAR (255) NOT NULL,
     icon_name VARCHAR (255) NOT NULL,
     icon_color VARCHAR (255) NOT NULL,
 
-    PRIMARY KEY(category_id)
+    PRIMARY KEY(id)
 );
 
 CREATE TABLE expenses
 (
-    expense_id INT GENERATED ALWAYS AS IDENTITY,
-    category_id INT REFERENCES categories(category_id),
-    collection_id INT REFERENCES collections(collection_id),
-    user_id INT REFERENCES users(user_id),
+    id INT GENERATED ALWAYS AS IDENTITY,
+    category_id INT REFERENCES categories(id),
+    collection_id INT REFERENCES collections(id),
+    user_id INT REFERENCES users(id),
     name VARCHAR (255) NOT NULL,
     date TIMESTAMPTZ NOT NULL,
     price INT NOT NULL,
 
-    PRIMARY KEY(expense_id)
+    PRIMARY KEY(id)
 );
