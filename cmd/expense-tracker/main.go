b1994f27d83286afb37e300917a179c5c23577ff
Romain Bretécher <romain.bretecher@gmail.com>
2020-11-11 00:30:30 +0100

    refactor: move app into dedicated package

--- cmd/expense-tracker/main.go
+++ cmd/expense-tracker/main.go
@@ -1,24 +1,10 @@
 package main
 
 import (
-	"net/http"
-
-	"github.com/gorilla/rpc/v2"
-	"github.com/gorilla/rpc/v2/json2"
-	"github.com/rbretecher/expense-tracker-back/internal/infrastructure/database"
-	"github.com/rbretecher/expense-tracker-back/internal/service"
-	"github.com/rbretecher/expense-tracker-back/pkg/validator"
+	"github.com/rbretecher/expense-tracker-back/internal/app"
 )
 
 func main() {
-	db := database.Connect()
-
-	s := rpc.NewServer()
-	s.RegisterCodec(json2.NewCodec(), "application/json")
-	s.RegisterCodec(json2.NewCodec(), "application/json;charset=UTF-8")
-	s.RegisterValidateRequestFunc(validator.Validate)
-	s.RegisterService(service.NewCollectionService(db), "Collection")
-
-	http.Handle("/rpc", s)
-	http.ListenAndServe(":8080", nil)
+	app := app.New()
+	app.Start()
 }
--- pkg/validator/validator.go
+++ pkg/validator/validator.go
@@ -2,11 +2,6 @@ package validator
 
 import (
 	"github.com/go-playground/validator/v10"
-	"github.com/gorilla/rpc/v2"
 )
 
-var validate *validator.Validate = validator.New()
-
-func Validate(r *rpc.RequestInfo, i interface{}) error {
-	return validate.Struct(i)
-}
+var Validate *validator.Validate = validator.New()
