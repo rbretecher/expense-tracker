8fb5c7b891f60417627158f6dd2e89af438a2b99
Romain Bretécher <romain.bretecher@gmail.com>
2020-11-15 00:23:26 +0100

    feat: add some fixtures

--- docker-entrypoint-initdb.d/init.sql
+++ docker-entrypoint-initdb.d/init.sql
@@ -51,3 +51,10 @@ CREATE TABLE user_has_collection
 
     PRIMARY KEY(user_id, collection_id)
 );
+
+INSERT INTO users (name, email, password, admin, icon_name, icon_color) VALUES ('Romain', 'admin@demo.com', 'azerty', true, 'face', 'blue-1');
+
+INSERT INTO categories (name, icon_name, icon_color) VALUES ('Groceries', 'local_grocery_store', 'blue-10');
+INSERT INTO categories (name, icon_name, icon_color) VALUES ('Housing', 'hotel', 'red-10');
+INSERT INTO categories (name, icon_name, icon_color) VALUES ('Car', 'local_taxi', 'green-10');
+INSERT INTO categories (name, icon_name, icon_color) VALUES ('Food', 'local_pizza', 'brown-10');
