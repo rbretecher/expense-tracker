1c3a5e3e5fedfd337358ecefa1c8d720cf86d61d
Romain Bretécher <romain.bretecher@gmail.com>
2020-11-19 19:51:46 +0100

    chore(db): add cascades and some new fixtures

--- docker-entrypoint-initdb.d/init.sql
+++ docker-entrypoint-initdb.d/init.sql
@@ -34,9 +34,9 @@ CREATE TABLE categories
 CREATE TABLE expenses
 (
     id INT GENERATED ALWAYS AS IDENTITY,
-    category_id INT REFERENCES categories(id),
-    collection_id INT REFERENCES collections(id),
-    user_id INT REFERENCES users(id),
+    category_id INT REFERENCES categories(id) ON DELETE SET NULL,
+    collection_id INT REFERENCES collections(id) ON DELETE CASCADE,
+    user_id INT REFERENCES users(id) ON DELETE CASCADE,
     name VARCHAR (255) NOT NULL,
     date TIMESTAMPTZ NOT NULL,
     price INT NOT NULL,
@@ -46,15 +46,25 @@ CREATE TABLE expenses
 
 CREATE TABLE user_has_collection
 (
-    user_id INT REFERENCES users(id),
-    collection_id INT REFERENCES collections(id),
+    user_id INT REFERENCES users(id) ON DELETE CASCADE,
+    collection_id INT REFERENCES collections(id) ON DELETE CASCADE,
 
     PRIMARY KEY(user_id, collection_id)
 );
 
-INSERT INTO users (name, email, password, admin, icon_name, icon_color) VALUES ('Romain', 'admin@demo.com', 'azerty', true, 'face', 'blue-1');
+INSERT INTO users (name, email, password, admin, icon_name, icon_color) VALUES ('Romain', 'admin@demo.com', 'azerty', true, 'hotel', 'blue-1');
+INSERT INTO users (name, email, password, admin, icon_name, icon_color) VALUES ('Madeline', 'madeline@demo.com', 'azerty', true, 'local_pizza', 'pink-1');
 
 INSERT INTO categories (name, icon_name, icon_color) VALUES ('Groceries', 'local_grocery_store', 'blue-10');
 INSERT INTO categories (name, icon_name, icon_color) VALUES ('Housing', 'hotel', 'red-10');
 INSERT INTO categories (name, icon_name, icon_color) VALUES ('Car', 'local_taxi', 'green-10');
 INSERT INTO categories (name, icon_name, icon_color) VALUES ('Food', 'local_pizza', 'brown-10');
+
+INSERT INTO collections (name, icon_name, icon_color) VALUES ('2018', 'hotel', 'blue-10');
+INSERT INTO collections (name, icon_name, icon_color) VALUES ('2019', 'hotel', 'blue-10');
+INSERT INTO collections (name, icon_name, icon_color) VALUES ('2020', 'hotel', 'blue-10');
+INSERT INTO collections (name, icon_name, icon_color) VALUES ('MDR', 'hotel', 'blue-10');
+
+INSERT INTO user_has_collection (user_id, collection_id) VALUES (1, 1);
+INSERT INTO user_has_collection (user_id, collection_id) VALUES (1, 2);
+INSERT INTO user_has_collection (user_id, collection_id) VALUES (1, 3);
