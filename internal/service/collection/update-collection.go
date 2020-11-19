f27f9588e6740a12b4d3492f837158f9e3b1b2de
Romain Bretécher <romain.bretecher@gmail.com>
2020-11-19 19:51:25 +0100

    feat: add update and delete collection actions

---
+++ internal/service/collection/update-collection.go
@@ -0,0 +1,26 @@
+package collection
+
+import (
+	"net/http"
+
+	"github.com/rbretecher/expense-tracker-back/internal/domain"
+	"github.com/rbretecher/expense-tracker-back/internal/service"
+)
+
+type UpdateArgs struct {
+	ID        int    `json:"id" validate:"required"`
+	Name      string `json:"name" validate:"required"`
+	IconName  string `json:"iconName" validate:"required"`
+	IconColor string `json:"iconColor" validate:"required"`
+}
+
+func (s *CollectionService) Update(r *http.Request, args *UpdateArgs, reply *domain.Collection) error {
+	err := s.db.QueryRow(`
+		UPDATE collections
+		SET name = $2, icon_name = $3, icon_color = $4
+		WHERE id = $1
+		RETURNING id, name, icon_name, icon_color
+	`, args.ID, args.Name, args.IconName, args.IconColor).Scan(&reply.ID, &reply.Name, &reply.IconName, &reply.IconColor)
+
+	return service.HandleUpdate(err)
+}
