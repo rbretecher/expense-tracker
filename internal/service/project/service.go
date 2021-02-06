7202baf2bf6cbde901fb30d16fca5ae3291fb610
Romain Bretécher <romain.bretecher@gmail.com>
2021-02-06 18:30:07 +0100

    feat: add Project.GetMonth and Project.GetSummary methods

--- internal/service/project/service.go
+++ internal/service/project/service.go
@@ -2,6 +2,7 @@ package project
 
 import (
 	"github.com/jmoiron/sqlx"
+	"github.com/rbretecher/expense-tracker-back/internal/domain"
 	"github.com/rbretecher/expense-tracker-back/internal/service"
 )
 
@@ -15,4 +16,26 @@ func NewService(db *sqlx.DB) *ProjectService {
 			DB: db,
 		},
 	}
+}
+
+func (s *ProjectService) getProject(projectID int) (project domain.Project, err error) {
+	err = s.DB.Get(&project, `
+		SELECT p.*, (SELECT COUNT(*) FROM expenses WHERE project_id = p.id) AS count
+		FROM projects p
+		WHERE p.id = $1;
+	`, projectID)
+
+	return
+}
+
+func (s *ProjectService) getProjectUsers(projectID int) ([]*domain.User, error) {
+	users := make([]*domain.User, 0)
+	err := s.DB.Select(&users, `
+		SELECT u.*
+		FROM users u
+		JOIN user_has_project uhp ON (u.id = uhp.user_id)
+		WHERE uhp.project_id = $1;
+	`, projectID)
+
+	return users, err
 }
