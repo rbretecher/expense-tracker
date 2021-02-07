cbdaca93a77473388fb8a40ec3afe61bb616c305
Romain Bretécher <romain.bretecher@gmail.com>
2021-02-07 17:25:42 +0100

    feat: use DATABASE_URL env variable to connect to PG database

--- internal/infrastructure/database/pg.go
+++ internal/infrastructure/database/pg.go
@@ -1,7 +1,6 @@
 package database
 
 import (
-	"fmt"
 	"log"
 	"os"
 
@@ -10,16 +9,7 @@ import (
 )
 
 func Connect() *sqlx.DB {
-	db, err := sqlx.Connect(
-		"postgres",
-		fmt.Sprintf(
-			"host=%s user=%s password=%s dbname=%s sslmode=disable",
-			os.Getenv("POSTGRES_HOST"),
-			os.Getenv("POSTGRES_USER"),
-			os.Getenv("POSTGRES_PASSWORD"),
-			os.Getenv("POSTGRES_DB"),
-		),
-	)
+	db, err := sqlx.Connect("postgres", os.Getenv("DATABASE_URL"))
 
 	if err != nil {
 		log.Fatalln(err)
