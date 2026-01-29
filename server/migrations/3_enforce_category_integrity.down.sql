1fd9a01d23eeba59b32670d07302d491a815799c
Romain Bretécher <romain.bretecher@gmail.com>
2026-01-29 15:05:47 +0100

    fix: add missing down migration and fix format

--- server/migrations/3_enforce_category_integrity.down.sql
+++ server/migrations/3_enforce_category_integrity.down.sql
@@ -3,19 +3,19 @@ ALTER TABLE expenses
     ADD CONSTRAINT expenses_category_id_fkey
         FOREIGN KEY (category_id)
         REFERENCES categories(id)
-        ON DELETE RESTRICT;
+        ON DELETE SET NULL;
 
 ALTER TABLE recurring_expenses
     DROP CONSTRAINT recurring_expenses_category_id_fkey,
     ADD CONSTRAINT recurring_expenses_category_id_fkey
         FOREIGN KEY (category_id)
         REFERENCES categories(id)
-        ON DELETE RESTRICT;
+        ON DELETE SET NULL;
 
 ALTER TABLE expenses
     ALTER COLUMN category_id
-    SET NOT NULL;
+    DROP NOT NULL;
 
 ALTER TABLE recurring_expenses
     ALTER COLUMN category_id
-    SET NOT NULL;
+    DROP NOT NULL;
